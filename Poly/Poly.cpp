aa82bcdfeeda21ea20d9394d0c6739c2daa25b47
Kamil Özdemir <kamilozdemir4972@gmail.com>
2025-11-01 01:44:19 +0300

    AddNode fonksiyonu oluşturuldu

---
+++ Poly/Poly.cpp
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include "Poly.h"
+
+//-------------------------------------------------
+// Creates a polynomial from an expression.
+// Returns a pointer to the first PolyNode in the list (head of the list)
+//
+// Expression will be of the following sort:
+// Ex1: 2.3x^4 + 5x^3 - 2.64x - 4
+// Ex2: -4.555x^10 - 45.44
+// Ex3: x^6 + 2x^4 - x^3 - 6.3x + 4.223 
+// Ex4: 34
+// Ex5: -x+1
+// Ex6: -3x^4    +   4x
+// Ex7: -2x  - 5
+//
+PolyNode *CreatePoly(char *expr){		
+	return NULL;
+} //end-CreatePoly
+
+/// -------------------------------------------------
+/// Walk over the poly nodes & delete them
+///
+void DeletePoly(PolyNode* poly) {
+	// Fill this in
+} // end-DeletePoly
+	
+//-------------------------------------------------
+// Adds a node (coefficient, exponent) to poly. If there already 
+// is a node with the same exponent, then you simply add the coefficient
+// to the existing coefficient. If not, you add a new node to polynomial
+// Returns a pointer to the possibly new head of the polynomial.
+//
+PolyNode* AddNode(PolyNode *head, double coef, int exp){
+	// yeni node'u oluştur
+	PolyNode* newNode = new PolyNode;
+	newNode->coef = coef;
+	newNode->exp = exp;
+	newNode->next = NULL;
+	// eğer liste boşsa yeni node'u başa ekle ve onu döndür
+	if (head == NULL) {
+		if (coef == 0) {
+			delete newNode;
+			return NULL;
+		}
+		return newNode;
+	}
+	// eğer yeni node'un üssü baştaki node'un üssünden büyükse, yeni node'u başa ekle
+	if(exp > head->exp) {
+		newNode->next = head;
+		return newNode;
+	}
+	// eğer üsler eşitse katsayıları topla 0'a özdel durum biçilir.
+	if(exp == head->exp){
+		double newCoef = head->coef + coef;
+		delete newNode; // artık head ile devam edebiliriz
+		if(newCoef == 0){
+			PolyNode* temp = head->next;
+			delete head;
+			return temp;// listenin kalanını kaybetmemek için
+		}else{
+			head->coef = newCoef;// katsayı güncellendi
+			return head;
+		}
+	}
+	// liste boş değilse direkt head ile işimiz yoksa listeyi dolaşmamız gerekir iki işaretçiyle listeyi dolaşacağız (prev ve current)
+	PolyNode* prev = head;
+	PolyNode* current = head->next;
+
+	while(current != NULL){
+		if (exp > current->exp){
+			// aradığımız yer burası
+			break;
+		}
+		if(exp == current->exp){
+			double newCoef = current->coef + coef;
+			delete newNode; // artık current ile devam edebiliriz
+			if(newCoef == 0){
+				prev->next = current->next;
+				delete current;	// current node'una artık ihtiyacımız kalmadı
+			}else{
+				current->coef = newCoef; // katsayı güncellendi
+			}
+			return head; // head değişmedi
+		}
+		prev = current;
+		current = current->next;		
+	}
+	// current ya NULL idi ya da en küçük üsse sahipti
+	// yeni node'u prev'in sonrasına ekle
+	prev->next = newNode;
+	newNode->next = current;
+	return head;// head değişmedi
+
+} // end-AddNode
+
+//-------------------------------------------------
+// Adds two polynomials and returns a new polynomial that contains the result
+// Computes: poly3 = poly1 + poly2 and returns poly3
+//
+PolyNode *Add(PolyNode *poly1, PolyNode *poly2){
+	// Fill this in
+	return NULL;
+} //end-Add
+
+//-------------------------------------------------
+// Subtracts poly2 from poly1 and returns the resulting polynomial
+// Computes: poly3 = poly1 - poly2 and returns poly3
+//
+PolyNode *Subtract(PolyNode *poly1, PolyNode *poly2){
+	// Fill this in
+	return NULL;
+} //end-Substract
+
+//-------------------------------------------------
+// Multiplies poly1 and poly2 and returns the resulting polynomial
+// Computes: poly3 = poly1 * poly2 and returns poly3
+//
+PolyNode *Multiply(PolyNode *poly1, PolyNode *poly2){
+	// Fill this in
+	return NULL;
+} //end-Multiply
+
+//-------------------------------------------------
+// Evaluates the polynomial at a particular "x" value and returns the result
+//
+double Evaluate(PolyNode *poly, double x){
+	// Fill this in
+	return 0;
+} //end-Evaluate
+
+//-------------------------------------------------
+// Computes the derivative of the polynomial and returns it
+// Ex: poly(x) = 3x^4 - 2x + 1-->Derivative(poly) = 12x^3 - 2
+//
+PolyNode *Derivative(PolyNode *poly){
+	// Fill this in
+	return NULL;
+} //end-Derivative
+
+//-------------------------------------------------
+// Plots the polynomial in the range [x1, x2].
+// -39<=x1<x2<=39
+// -12<=y<=12
+// On the middle of the screen you gotta have x & y axis plotted
+// During evaluation, if "y" value does not fit on the screen,
+// then just skip it. Otherwise put a '*' char depicting the curve
+//
+void Plot(PolyNode *poly, int x1, int x2){
+	// Fill this in	
+} //end-Plot
